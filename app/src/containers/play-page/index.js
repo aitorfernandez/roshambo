ec8d4e2db3266c017fa56f5048a86033dad2e020
Aitor Fernandez <helloimaitor@gmail.com>
2021-07-02 07:51:50 +0100

    add PlayPage controller queries and mutations

--- app/src/containers/play-page/index.js
+++ app/src/containers/play-page/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import {
+  gql,
+  useMutation,
+  useQuery,
+} from '@apollo/client'
 
+import { useCookies } from 'react-cookie'
 import { AppContent } from '../../components/app-content'
 import { Header } from '../../components/header'
 import { Profile } from '../../components/profile'
@@ -10,6 +16,33 @@ import { StatResult } from '../../components/stat-result'
 import { StatScore } from '../../components/stat-score'
 
 export function PlayPage() {
+  const [{ id }] = useCookies(['id'])
+
+  const { data, loading, error } = useQuery(PlayPageAccountQuery, {
+    variables: {
+      id,
+    },
+  })
+
+  const [createStat] = useMutation(PlayPageCreateStatMutation, {
+    update: (cache, { data }) => {
+      console.log(cache, data)
+    },
+  })
+  const [deleteStats] = useMutation(PlayPageDeleteStatsMutation, {
+    update: (cache) => {
+      console.log(cache)
+    },
+  })
+
+  if (loading) {
+    return null
+  }
+  if (error) {
+    console.error(error)
+    return null
+  }
+
   return (
     <AppContent
       header={
@@ -19,15 +52,43 @@ export function PlayPage() {
         </>
       }
     >
-      <StatResult />
-      <StatScore />
+      <StatResult { ...data.account } />
+      <StatScore { ...data.account } />
       <div>
-        <StatCreateButton />
-        <StatCreateButton />
-        <StatCreateButton />
-        <StatDeleteButton />
+        <StatCreateButton { ...{ createStat } } />
+        <StatCreateButton { ...{ createStat } } />
+        <StatCreateButton { ...{ createStat } } />
+        <StatDeleteButton { ...{ deleteStats } } />
       </div>
       <StatItem />
     </AppContent>
   )
 }
+
+const PlayPageAccountQuery = gql`
+  query PlayPageAccountQuery(
+    $id: ID!
+  ) {
+    account(id: $id) {
+      id
+    }
+  }
+`
+
+const PlayPageCreateStatMutation = gql`
+  mutation PlayPageCreateStatMutation(
+    $input: CreateStatInput!
+  ) {
+    stat: createStat(input: $input) {
+      id
+    }
+  }
+`
+
+const PlayPageDeleteStatsMutation = gql`
+  mutation PlayPageDeleteStatsMutation(
+    $accountID: ID!
+  ) {
+    deleteStatsByAccount(accountID: $accountID)
+  }
+`
