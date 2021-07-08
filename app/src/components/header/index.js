d6e41d2b871ca516f725e5c29c41de2791395376
Aitor Fernandez <helloimaitor@gmail.com>
2021-07-08 14:37:22 +0100

    add header styles

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { t } from '../../theme'
+import {
+  Flex,
+  Icon,
+  Link,
+} from '../../ui'
 
 export function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <Link to="/">
+      <Flex>
+        {['rock', 'paper', 'scissors'].map((item) => (
+          <StyledIcon
+            c="white.5"
+            fs={2}
+            icon={`hand-${item}`}
+            key={item}
+          />
+        ))}
+      </Flex>
+    </Link>
   )
 }
+
+const StyledIcon = styled(Icon)`
+  margin: 0 ${t('space.2')};
+`
